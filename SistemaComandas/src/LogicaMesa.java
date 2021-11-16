3fb3756fdc884d77351569d42322710e202b3ae0
dsantosc04 <dsantosc04@infolab02-w27.instituto.extremadura.es>
2021-11-16 19:24:21 +0100

    Mesas DefaultTableModel added.

--- SistemaComandas/src/LogicaMesa.java
+++ SistemaComandas/src/LogicaMesa.java
@@ -8,30 +8,43 @@ import javax.swing.JOptionPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author dsantosc04
  */
 public class LogicaMesa {
-    
+
     private List<Mesa> listaMesas;
     private VentanaPrincipal ventanaPrincipal;
-    
-    public LogicaMesa(VentanaPrincipal ventanaPrincipal){
+
+    public LogicaMesa(VentanaPrincipal ventanaPrincipal) {
         this.listaMesas = new ArrayList<>();
         this.ventanaPrincipal = ventanaPrincipal;
     }
-    
-    public void aniadirMesa(Mesa mesa){
-        listaMesas.add(mesa);
-        JOptionPane.showMessageDialog(ventanaPrincipal, "Se ha añadido correctamente la mesa");
+
+    public void aniadirMesa(Mesa mesa) {
+        //Comprobamos antes que no existe esa mesa
+
+        if (comprobarExisteMesa(mesa)) {
+            JOptionPane.showMessageDialog(ventanaPrincipal, "Esta mesa ya existe", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } else {
+            listaMesas.add(mesa);
+            JOptionPane.showMessageDialog(ventanaPrincipal, "Se ha añadido correctamente la mesa");
+        }
+
     }
 
     public List<Mesa> getListaMesas() {
         return listaMesas;
     }
-    
-    
-    
+
+    public boolean comprobarExisteMesa(Mesa mesa) {
+        for (Mesa listaMesa : listaMesas) {
+            if (listaMesa.getNumMesa() == mesa.getNumMesa()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
